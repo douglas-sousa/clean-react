d168a521543d3c505c8ddbdf717a197bc02459d0
Douglas <douglasrdsousa1@gmail.com>
2020-10-06 20:50:58 -0300

    refactor: move factories to their own files

---
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,7 @@
+import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication';
+import { makeAxiosHtttpClient } from '@/main/factories/http/axios-http-client-factory';
+import { makeApiUrl } from '@/main/factories/http/api-url-factory';
+
+export const makeRemoteAuthentication = (): RemoteAuthentication => {
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHtttpClient());
+}
