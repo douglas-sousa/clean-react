a7bd13fbca8d86335e3dec0e338643b392b3c9ef
Douglas <douglasrdsousa1@gmail.com>
2020-10-10 10:35:05 -0300

    refactor: eslint's configuration change

--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -1,7 +1,7 @@
-import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication';
-import { makeAxiosHtttpClient } from '@/main/factories/http/axios-http-client-factory';
-import { makeApiUrl } from '@/main/factories/http/api-url-factory';
+import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
+import { makeAxiosHtttpClient } from '@/main/factories/http/axios-http-client-factory'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 
 export const makeRemoteAuthentication = (): RemoteAuthentication => {
-  return new RemoteAuthentication(makeApiUrl(), makeAxiosHtttpClient());
+  return new RemoteAuthentication(makeApiUrl(), makeAxiosHtttpClient())
 }
